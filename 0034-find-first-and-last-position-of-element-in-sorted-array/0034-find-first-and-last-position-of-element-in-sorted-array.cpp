3b5740547cf5b0d7715f9e588c86cdc3a36787d7
ambadeanshul <ambadeanshul@gmail.com>
2026-08-18 16:23:34 +0530

    Time: 0 ms (100%), Space: 17.5 MB (89.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,44 +4,46 @@ public:
         int n=nums.size();
         int l=0;
         int h=n-1;
-        int first=-1;
         
+        
+        int index=-1;
         while(l<=h){
             int m=l+(h-l)/2;
-            
+            if(nums[m]==target){ 
+                index=m;
+                break;
+                }
+            if(nums[m]<target) l=m+1;
+            else h=m-1;
+        }
+        if(index==-1){
+            return {-1,-1};
+        }
+        int first=index;
+        int second=index;
+        l=0;
+        h=index-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
             if(nums[m]==target){
                 first=m;
                 h=m-1;
-
             }
-            
-            else if(nums[m]>target){
-                h=m-1;
-            }
-            else l=m+1;
-
+            else if(nums[m]<target) l=m+1;
+            else h=m-1;
         }
-         l=0;
-         h=n-1;
-        int second=-1;
 
+        l=index+1;
+        h=n-1;
         while(l<=h){
             int m=l+(h-l)/2;
-            
             if(nums[m]==target){
                 second=m;
                 l=m+1;
-
             }
-            
-            else if(nums[m]>target){
-                h=m-1;
-            }
-            else l=m+1;
-
+            else if(nums[m]<target) l=m+1;
+            else h=m-1;
         }
         return {first,second};
-        
     }
-
 };
