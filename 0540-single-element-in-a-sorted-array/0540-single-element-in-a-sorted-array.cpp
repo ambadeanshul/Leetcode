7b9d75f8ba3272d0f405b7686dd581b678ffaf45
ambadeanshul <ambadeanshul@gmail.com>
2026-08-18 21:46:36 +0530

    Time: 0 ms (100%), Space: 26.3 MB (36.5%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -18,7 +18,7 @@ public:
                 if( m>=1 && nums[m]==nums[m-1] ){
                     l=m+1;
                 }
-                else if(m<=n-1 && nums[m]==nums[m+1]) h=m-1;
+                else if(m<=n-2 && nums[m]==nums[m+1]) h=m-1;
                 else return nums[m];
             }
         }
